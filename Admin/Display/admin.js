ce089d39b754e2fa2017ca7c98851713e5920e48
Michael Toppa <public@toppa.com>
2011-10-05 21:55:24 -0400

    simplified jQuery code

--- Admin/Display/admin.js
+++ Admin/Display/admin.js
@@ -1,34 +1,17 @@
-/**
- * Accepts variables passed in from Shashin, and customizes the display of Highslide.
- *
- * This file is part of Shashin. Please see the Shashin.phl file for
- * copyright and license information.
- *
- * @author Michael Toppa
- * @version 3.0
- */
-
-
 jQuery(document).ready(function($) {
-    jQuery.shashin_toggle = function(base_name) {
-        if ($('#' + base_name + '_section').is(':visible')) {
-            $('#' + base_name + '_button').attr('src', shashinDisplay.url + '/images/plus.gif');
-            $('#' + base_name + '_section').hide('slow');
+    $('#shashinExamples').click(function(event) {
+        if ($('#' + $(this).attr('id') + 'Section').is(':visible')) {
+            $('#' + $(this).attr('id') + 'Button').attr('src', shashinDisplay.url + '/images/plus.gif');
+            $('#' + $(this).attr('id') + 'Section').hide('slow');
         }
 
         else {
-            $('#' + base_name + '_button').attr('src', shashinDisplay.url + '/images/minus.gif');
-            $('#' + base_name + '_section').show('slow');
+            $('#' + $(this).attr('id') + 'Button').attr('src', shashinDisplay.url + '/images/minus.gif');
+            $('#' + $(this).attr('id') + 'Section').show('slow');
         }
 
-        // returning false disables the href action
-        return false;
-    };
-
-    $('#shashin_main').click(function () { return $.shashin_toggle('shashin_main'); });
-    $('#shashin_picasa').click(function () { return $.shashin_toggle('shashin_picasa'); });
-    $('#shashin_highslide').click(function () { return $.shashin_toggle('shashin_highslide'); });
-    $('#shashin_examples').click(function () { return $.shashin_toggle('shashin_examples'); });
+        event.preventDefault();
+    });
 });
 
 
