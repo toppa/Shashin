b62b79649f9c190c1103a9d404839f9b4dc02f9c
Michael Toppa <public@toppa.com>
2011-10-05 21:58:04 -0400

    added run()

--- Admin/ShashinHeadTags.php
+++ Admin/ShashinHeadTags.php
@@ -2,10 +2,23 @@
 
 
 class Admin_ShashinHeadTags {
+    private $version;
     private $functionsFacade;
 
-    public function __construct(ToppaFunctionsFacade $functionsFacade) {
-        $this->functionsFacade = $functionsFacade;
+    public function __construct($version) {
+        $this->version = $version;
     }
 
+    public function setFunctionsFacade(ToppaFunctionsFacade $functionsFacade) {
+        $this->functionsFacade = $functionsFacade;
+        return $this->functionsFacade;
+    }
+    public function run() {
+        $cssUrl = $this->functionsFacade->getUrlforCustomizableFile('admin.css', __FILE__, 'Display/');
+        $this->functionsFacade->enqueueStylesheet('shashinAdminStyle', $cssUrl, false, $this->version);
+        $jsUrl = $this->functionsFacade->getUrlforCustomizableFile('admin.js', __FILE__, 'Display/');
+        $this->functionsFacade->enqueueScript('shashinAdminScript', $jsUrl, array('jquery'), $this->version);
+        $menuDisplayUrl = $this->functionsFacade->getPluginsUrl('/Display/', __FILE__);
+        $this->functionsFacade->localizeScript('shashinAdminScript', 'shashinDisplay', array('url' => $menuDisplayUrl));
+    }
 }
