9ca9c091636015417738a1ab7317543b17ea204a
Michael Toppa <public@toppa.com>
2012-02-28 18:38:55 -0500

    added Fancybox support for Twitpic and Youtube

---
+++ Public/ShashinPhotoDisplayerYoutubeFancybox.php
@@ -0,0 +1,47 @@
+<?php
+
+class Public_ShashinPhotoDisplayerYoutubeFancybox extends Public_ShashinPhotoDisplayerYoutube {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setImgTitle() {
+        $this->imgTitle = null;
+        return $this->imgTitle;
+    }
+
+    public function setLinkRel() {
+        $this->linkRel = 'shashinFancybox_' . $this->sessionManager->getGroupCounter();
+        return $this->linkRel;
+    }
+
+    public function setLinkRelVideo() {
+        return $this->setLinkRel();
+    }
+
+    // htmlspecialchars lets us put links within the title (but we want single quotes,
+    // which are not converted to entities)
+    public function setLinkTitle() {
+        $this->linkTitle = htmlspecialchars(str_replace('"', "'", $this->setDivOriginalPhotoLinkForCaption()))
+            . $this->functionsFacade->htmlSpecialCharsOnce($this->dataObject->description)
+            . htmlspecialchars(str_replace('"', "'", $this->setExifDataForCaption()));
+        return $this->linkTitle;
+    }
+
+    public function setLinkTitleVideo() {
+        $this->linkTitle = $this->functionsFacade->htmlSpecialCharsOnce($this->dataObject->description)
+            . ' - '
+            . htmlspecialchars(str_replace('"', "'", $this->setOriginalPhotoLinkForCaption()));
+        return $this->linkTitle;
+    }
+
+    public function setLinkClass() {
+        $this->linkClass = 'shashinFancybox';
+        return $this->linkClass;
+    }
+
+    public function setLinkClassVideo() {
+        $this->linkClass = 'shashinFancyboxVideo';
+        return $this->linkClass;
+    }
+}
