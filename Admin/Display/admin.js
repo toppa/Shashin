93655d4bf9a1eba116d99bd82452ba9f447d195a
Michael Toppa <public@toppa.com>
2012-06-16 14:15:58 -0400

    remove examples, add tabs

--- Admin/Display/admin.js
+++ Admin/Display/admin.js
@@ -1,17 +1,5 @@
 jQuery(document).ready(function($) {
-    $('#shashinExamples').click(function(event) {
-        if ($('#' + $(this).attr('id') + 'Section').is(':visible')) {
-            $('#' + $(this).attr('id') + 'Button').attr('src', shashinDisplay.url + '/images/plus.gif');
-            $('#' + $(this).attr('id') + 'Section').hide('slow');
-        }
-
-        else {
-            $('#' + $(this).attr('id') + 'Button').attr('src', shashinDisplay.url + '/images/minus.gif');
-            $('#' + $(this).attr('id') + 'Section').show('slow');
-        }
-
-        event.preventDefault();
-    });
+    $( "#shashinTabs" ).tabs();
 });
 
 
