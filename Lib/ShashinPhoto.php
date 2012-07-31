5fc8180e6d8974699af06d422f34b99a6c57bb1a
Michael Toppa <public@toppa.com>
2012-07-30 22:30:50 -0400

    moved refresh(), get() and flush() to parent class

--- Lib/ShashinPhoto.php
+++ Lib/ShashinPhoto.php
@@ -165,45 +165,11 @@ class Lib_ShashinPhoto extends Lib_ShashinDataObject {
         parent::__construct($dbFacade);
     }
 
-    public function get($id = null) {
-        // check a field we would have only if we have a fully constructed photo
-        if (!$this->data['sourceId']) {
-            return $this->refresh($id);
-        }
-
-        return $this->data;
-    }
-
-    public function refresh($id) {
-        if (!isset($id) || !is_numeric($id)) {
-            throw New Exception(__("Invalid photo key", "shashin"));
-        }
-
-        $where = array("id" => $id);
-        $this->data = $this->dbFacade->sqlSelectRow($this->tableName, null, $where);
-
-        if (empty($this->data)) {
-            throw New Exception(__("Failed to find database record for photo", "shashin"));
-        }
-
-        return $this->data;
-    }
-
     public function delete() {
         $this->dbFacade->sqlDelete($this->tableName, array('id' => $this->data['id']));
         $photoData = $this->data;
         $this->data = array(); // do not use unset
         return $photoData;
-    }
-
-    public function flush() {
-        $insertId = $this->dbFacade->sqlInsert($this->tableName, $this->data, true);
-
-        if (!$this->id) {
-            $this->id = $insertId;
-        }
-
-        return true;
     }
 
     public function isVideo() {
