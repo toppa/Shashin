9d7cd8b4eef41a38f7f02be9daa9077bb262ab43
Michael Toppa <public@toppa.com>
2012-07-30 22:30:15 -0400

    moved, refresh(), get() and flush() from child classes

--- Lib/ShashinDataObject.php
+++ Lib/ShashinDataObject.php
@@ -45,13 +45,33 @@ abstract class Lib_ShashinDataObject {
         throw New Exception(__('Invalid data property __set for ', 'shashin') . htmlentities($name));
     }
 
-    public function getData() {
+    public function refresh($id) {
+        if (!isset($id) || !is_numeric($id)) {
+            throw New Exception(__('Invalid ID', 'shashin'));
+        }
+
+        $where = array("id" => $id);
+        $this->data = $this->dbFacade->sqlSelectRow($this->tableName, null, $where);
+
+        if (empty($this->data)) {
+            throw New Exception(__('Failed to find database record for ID: ', 'shashin') . $id);
+        }
+
+        return $this->data;
+    }
+
+    public function get($id = null) {
+        // check a field we would have only if we have a fully constructed album or photo
+        if (!isset($this->data['sourceId'])) {
+            return $this->refresh($id);
+        }
+
         return $this->data;
     }
 
     public function set(array $fields) {
         if (!isset($fields) || empty($fields)) {
-            throw New Exception(__('set() must be caled with an array', 'shashin'));
+            throw New Exception(__('set() must be called with a populated array', 'shashin'));
         }
 
         $intTypes = $this->dbFacade->getIntTypes();
@@ -77,9 +97,16 @@ abstract class Lib_ShashinDataObject {
         return $this->data;
     }
 
-    abstract public function isVideo();
-    abstract public function get($key = null);
-    abstract public function refresh($key);
-    abstract public function flush();
+    public function flush() {
+        $insertId = $this->dbFacade->sqlInsert($this->tableName, $this->data, true);
+
+        if (!$this->id) {
+            $this->id = $insertId;
+        }
+
+        return true;
+    }
+
     abstract public function delete();
+    abstract public function isVideo();
 }
