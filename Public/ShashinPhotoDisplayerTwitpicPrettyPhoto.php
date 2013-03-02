061b19ed9ac9a7f489efe5d104e583823c927a70
Michael Toppa <public@toppa.com>
2013-03-02 12:31:07 -0500

    add prettyPhoto display subclasses

---
+++ Public/ShashinPhotoDisplayerTwitpicPrettyPhoto.php
@@ -0,0 +1,31 @@
+<?php
+
+class Public_ShashinPhotoDisplayerTwitpicPrettyPhoto extends Public_ShashinPhotoDisplayerPicasa {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setLinkRel() {
+        $groupNumber = $this->sessionManager->getGroupCounter();
+
+        if ($this->albumIdForAjaxPhotoDisplay) {
+            $groupNumber .= '_' . $this->albumIdForAjaxPhotoDisplay;
+        }
+
+        $this->linkRel .= "prettyPhoto[$groupNumber]";
+        return $this->linkRel;
+    }
+
+    public function setLinkRelVideo() {
+        return $this->setLinkRel();
+    }
+
+    public function setLinkTitle() {
+        $this->linkTitle = $this->functionsFacade->htmlSpecialCharsOnce($this->dataObject->description);
+        return $this->linkTitle;
+    }
+
+    public function setLinkTitleVideo() {
+        return $this->setLinkTitle();
+    }
+}
